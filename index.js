26b921573c5f29ff1233cfa59d87abab33b93e3a
zenglinxiang <zenglinxiang@yy.com>
2021-10-14 13:26:20 +0800

    feat: 添加机器人

---
+++ index.js
@@ -0,0 +1,56 @@
+const moment = require('moment')
+
+const axios = require('axios')
+
+// 获取是否是节假日
+const getIsWordDay = () => {
+    let today = moment().format('YYYYMMDD')
+    return new Promise((resolve, reject) => {
+        axios.get(`https://api.apihubs.cn/holiday/get?date=${today}`).then((res) => {
+            if (res.data.code == 0) {
+                let todayInfo = res.data.data.list[0]
+                let isWorkday = todayInfo.workday == 1
+                resolve(isWorkday)
+            }
+        }).catch(e => {
+            // 如果查询失败就不提醒
+            let isWorkday = false
+            resolve(isWorkday)
+        })
+    })
+}
+
+
+setInterval(() => {
+    const lunchTime = '13:14'
+    const dinnerTime = '18:19'
+    const time = moment().format('HH:mm')
+    if ([lunchTime, dinnerTime].includes(time)) {
+        getIsWordDay().then(isWorkday => {
+            if (isWorkday) {
+                console.log('吃饭了')
+                sendMsg()
+            }
+        })
+    }
+}, 60 * 1000)
+
+
+
+const sendMsg = () => {
+    axios.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=b2638742-7b3e-4f36-a68e-69c4fe2a2e65', {
+        msgtype: 'text',
+        text: {
+            content: '吃饭时间到了',
+            mentioned_list: ["@all"],
+        }
+    }, { 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+    }).then((res) => {
+        console.log(res.data)
+    })
+}
+
