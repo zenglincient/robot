7bb25e9cf4fcb1f5e21fafedff6fbe5a06eda875
zenglinxiang <zenglinxiang@joyy.com>
2021-12-25 11:32:13 +0800

    feat: 添加消息通知

--- message.js
+++ message.js
@@ -12,21 +12,16 @@ app.post('/api/sms', (req, res) => {
 
     console.log('post 请求')
     console.log(req.body)
-    res.send('ok')
-    // axios.post(url, data).then(response => {
-    //     res.send(response.data)
-    // })
-})
+    res.json({
+        code: 200,
+        meeeage: 'success'
+    })
+    res.send()
 
-
-app.get('/api/sms', (req, res) => {
-    console.log('get 请求')
-    console.log(req.query)
-    res.send('Hello World')
     axios.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d3c49d63-41cd-478a-830f-9e43e8836f03', {
         msgtype: 'text',
         text: {
-            content: '喂，三点几了，饮茶时间到了',
+            content: req.body && JSON.stringify(req.body),
             mentioned_list: ["@all"],
         }
     }, { 
@@ -37,6 +32,14 @@ app.get('/api/sms', (req, res) => {
     }).then((res) => {
         console.log(res.data)
     })
+})
+
+
+app.get('/api/sms', (req, res) => {
+    console.log('get 请求')
+    console.log(req.query)
+    res.send('Hello World')
+    
 })
 
 app.listen(9000, () => {
